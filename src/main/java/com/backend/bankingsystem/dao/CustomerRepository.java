8e0123896c925727cccc37c890817a9b0f46b71f
antema-andri <andrandrainaa@gmail.com>
2023-10-21 21:15:12 +0300

    commit

--- src/main/java/com/backend/bankingsystem/dao/CustomerRepository.java
+++ src/main/java/com/backend/bankingsystem/dao/CustomerRepository.java
@@ -3,5 +3,8 @@ package com.backend.bankingsystem.dao;
 import com.backend.bankingsystem.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    List<Customer> findByNameLike(String world);
 }
