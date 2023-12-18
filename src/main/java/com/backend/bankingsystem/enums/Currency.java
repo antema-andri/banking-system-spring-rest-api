471e35a163f0212f45265091d674b5bbcf4ded9f
antema-andri <andrandrainaa@gmail.com>
2023-12-18 19:33:04 +0300

    commit

--- src/main/java/com/backend/bankingsystem/enums/Currency.java
+++ src/main/java/com/backend/bankingsystem/enums/Currency.java
@@ -1,6 +1,16 @@
 package com.backend.bankingsystem.enums;
 
 public enum Currency {
-    EURO,
-    DOLLAR
+    EURO("EUR"),
+    DOLLAR("USD");
+
+    private String code;
+
+    private Currency(String code){
+        this.code=code;
+    }
+
+    public String getCode(){
+        return this.code;
+    }
 }
